5804e9c66be9100e276336c03556082786c95b43
Leon <leonkeutzer@gmail.com>
2024-11-20 15:00:55 +0100

    WebApp init

---
+++ webApp/frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5000/api/data')
+            .then((response) => response.json())
+            .then((data) => setData(data.message))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
+    return (
+        <div>
+            <h1>React + Flask WebApp</h1>
+            <p>{data ? data : 'Loading...'}</p>
+        </div>
+    );
+}
+
+export default App;
