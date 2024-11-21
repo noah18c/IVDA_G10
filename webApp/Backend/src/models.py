d419e1930fc867b772dbb8764b8420a7af7502fd
noah18c <noahcroes@gmail.com>
2024-11-22 00:59:39 +0100

    included rooms now too

--- webApp/Backend/src/models.py
+++ webApp/Backend/src/models.py
@@ -1,6 +1,9 @@
 class FurnitureItem:
 
-    def __init__(self, item_id, name, category, price, old_price, sellable_online, link, other_colors, short_description, designer, depth, height, width):
+    def __init__(
+        self, item_id, name, category, price, old_price, sellable_online, link, other_colors, short_description, 
+        designer, depth, height, width, living_room, bedroom, office, kitchen, dining_room, entrance, 
+        playroom, nursery, outdoor, space, size_cluster, size_category, cluster, rooms):        
         self.item_id = item_id
         self.name = name
         self.category = category
@@ -14,6 +17,20 @@ class FurnitureItem:
         self.depth = depth
         self.height = height
         self.width = width
+        self.living_room = living_room
+        self.bedroom = bedroom
+        self.office = office
+        self.kitchen = kitchen
+        self.dining_room = dining_room
+        self.entrance = entrance
+        self.playroom = playroom
+        self.nursery = nursery
+        self.outdoor = outdoor
+        self.space = space
+        self.size_cluster = size_cluster
+        self.size_category = size_category
+        self.cluster = cluster
+        self.rooms = rooms
 
     def __repr__(self):
         return f"<FurnitureItem {self.name} ({self.item_id})>"
